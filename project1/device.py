ec90a4afaecae3dfb9e29e7b2d2e9403c45e541c
Piratebird <allenandersonuwu@gmail.com>
2026-04-29 00:58:26 +0200

    bug fixes

--- project1/device.py
+++ project1/device.py
@@ -1,29 +1,42 @@
-import reader as r
-
-
 class Device:
     def __init__(self, device_type, name, ip, status, vlan=None):
-        self.device_type = device_type
+        self.device_type = device_type.strip().upper()
         self.name = name
         self.ip = ip
         self.status = status
         self.vlan = vlan
 
     def device_type_info(self):
-        if self.device_type.lower().strip() == "router":
-            self.name = "Router"
-            pass
-        elif self.device_type.lower().strip() == "switch":
-            pass
-        elif self.device_type.lower().strip() == "host":
-            pass
-        else:
-            return "Unknown device type."
+        device_labels = {
+            "HOST": "Host",
+            "SWITCH": "Switch",
+            "ROUTER": "Router",
+        }
+        return device_labels.get(self.device_type, "Unknown device type")
 
     def needs_attention(self):
-        return self.status.lower() in ["inactive", "maintenance"]
+        return self.status.lower().strip() in ["inactive", "maintenance"]
 
     def get_device_info(self):
+        device_class = self.device_type_info()
+
+        if self.device_type == "HOST":
+            return f"Device Class: {device_class}, IP: {self.ip}, Status: {self.status}"
+
         if self.device_type == "SWITCH":
-            return f"Device Type: {self.device_type}, Name: {self.name}, Status: {self.status}, VLAN: {self.vlan}"
-        return f"Device Type: {self.device_type}, Name: {self.name}, IP: {self.ip}, Status: {self.status}"
+
+            return (
+                f"Device Class: {device_class}, Name: {self.name}, "
+                f"Status: {self.status}, VLAN: {self.vlan}"
+            )
+
+        if self.device_type == "ROUTER":
+            return (
+                f"Device Class: {device_class}, Name: {self.name}, "
+                f"IP: {self.ip}, Status: {self.status}"
+            )
+
+        return (
+            f"Device Class: {device_class}, Name: {self.name}, "
+            f"IP: {self.ip}, Status: {self.status}"
+        )
